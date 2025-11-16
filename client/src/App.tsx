2707deb92887fca03314171213e340c169f5340b
cankuteskin@gmail.com <cankuteskin@gmail.com>
2025-11-16 22:05:28 +0300

    chore: add /dpad-chooser route for DPad preview (temporary)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,11 +4,13 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Game from "@/pages/Game";
+import DPadChooser from "@/pages/DPadChooser";
 
 function Router() {
   return (
     <Switch>
       <Route path="/" component={Game} />
+      <Route path="/dpad-chooser" component={DPadChooser} />
     </Switch>
   );
 }
